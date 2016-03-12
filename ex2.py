e8ef13b0a9be35ae7a6accfedbb7e0e0e5ed845d
linus1211 <linus1211@163.com>
2016-03-12 20:47:07 +0800

    ex2.py

--- ex2.py
+++ ex2.py
@@ -1,17 +1,17 @@
 # A comment, this is so you can read your program later.
 # Anything after the # is ignored by python.
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 print ("I could have code like this.") # and the comment after is ignored
-=======
-print "I could have code like this." # and the comment after is ignored
->>>>>>> daa63bde53691352c6eb66d5c17bed155f738e40
+#=======
+#print "I could have code like this." # and the comment after is ignored
+#>>>>>>> daa63bde53691352c6eb66d5c17bed155f738e40
 
 # You can also use a comment to "disable" or comment out a piece of code:
 # print "This won't run."
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 print ("This will run.")
-=======
-print "This will run."
->>>>>>> daa63bde53691352c6eb66d5c17bed155f738e40
+#=======
+#print "This will run."
+#>>>>>>> daa63bde53691352c6eb66d5c17bed155f738e40
