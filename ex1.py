21837b85e40b17cc6f80894049a3da201690eae9
linus1211 <linus1211@163.com>
2016-03-12 20:41:46 +0800

    ex1.py

--- ex1.py
+++ ex1.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 print ("Hello World!")
 print ("Hello Again")
 print ("I like typing this.")
@@ -7,13 +7,4 @@ print ('Yay! Printing.')
 print ("I'd much rather you 'not'.")
 # print 'I "said" do not touch this.'
 print ('This is another line!')
-=======
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-# print 'I "said" do not touch this.'
-print 'This is another line!'
->>>>>>> daa63bde53691352c6eb66d5c17bed155f738e40
+
