c33c9fcb30f926ac502725fb8a3058841f2175c3
linus1211 <linus1211@163.com>
2016-03-15 21:15:18 +0800

    0315

--- ex26-test.py
+++ ex26-test.py
@@ -1,3 +1,4 @@
+import ex25
 def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
@@ -34,26 +35,24 @@ def print_first_and_last_sorted(sentence):
     print_first_word(words)
     print_last_word(words)
 
-
 print ("Let's practice everything.")
 print ('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
-
 poem = """
 \tThe lovely world
 with logic so firmly planted
 cannot discern \n the needs of love
 nor comprehend passion from intuition
-and requires an explanation
+and requires an explantion
 \n\t\twhere there is none.
 """
 
 
-print "--------------"
-print poem
-print "--------------"
+print ("--------------")
+print (poem)
+print ("--------------")
 
-five = 10 - 2 + 3 - 6
-print "This should be five: %s" % five
+five = 10 - 2 + 3 - 5
+print ("This should be five: %s" % five)
 
 def secret_formula(started):
     jelly_beans = started * 500
@@ -64,18 +63,14 @@ def secret_formula(started):
 
 start_point = 10000
 beans, jars, crates = secret_formula(start_point)
-
-print "With a starting point of: %d" % start_point
-print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
+print ("With a starting point of: %d" % start_point)
+print ("We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates))
 
 start_point = start_point / 10
+print ("We can also do that this way:")
+print ("We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point))
 
-print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
-
-
-sentence = "All good things come to those who wait."
-
+sentence = "All god\tthings come to those who weight."
 words = break_words(sentence)
 sorted_words = sort_words(words)
 
@@ -84,8 +79,9 @@ print_last_word(words)
 print_first_word(sorted_words)
 print_last_word(sorted_words)
 sorted_words = sort_sentence(sentence)
-print sorted_words
+print (sorted_words)
 
 print_first_and_last(sentence)
-
 print_first_and_last_sorted(sentence)
+
+
